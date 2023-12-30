099daad3f4707d086f6e2646c82df7a199ba4d62
alex800121 <alex800121@hotmail.com>
2023-12-31 02:16:43 +0800

    2018 init

---
+++ src/Day1.hs
@@ -0,0 +1,6 @@
+module Day1 where
+
+day1 :: IO ()
+day1 = do
+  -- input <- readFile "input/input1.txt"
+  return ()
---
+++ src/Day2.hs
@@ -0,0 +1,6 @@
+module Day2 where
+
+day2 :: IO ()
+day2 = do
+  -- input <- readFile "input/input2.txt"
+  return ()
