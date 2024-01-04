bc7801e907774151aaff34792fcd4686a6d2dcbe
alex800121 <alex800121@hotmail.com>
2024-01-05 06:57:03 +0800

    day12

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,6 +1,62 @@
+{-# LANGUAGE LambdaCase #-}
 module Day10 where
+
+import Data.Maybe (mapMaybe)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import MyLib
+import Text.Megaparsec (many, optional, parseMaybe, parseTest, some)
+import Text.Megaparsec.Char (char, space, string)
+import Debug.Trace
+import qualified Data.Map.Strict as Map
+import Data.Bifunctor (Bifunctor(..))
+
+type Index = (Int, Int)
+
+type Velo = (Int, Int)
+
+type Light = (Index, Velo)
+
+type Graph = Set Light
+
+calcGraph :: Int -> Graph -> (Int, [Graph])
+calcGraph r g = (n, map (gs !!) [n - r .. n + r])
+  where
+    gs = iterate next g
+    f n (x : y : xs)
+      | calcDist x >= calcDist y = f (n + 1) (y : xs)
+      | otherwise = n
+    n = f 0 gs
+
+calcDist :: Graph -> Int
+calcDist g = (maxX - minX) + (maxY - minY)
+  where
+    (xs, ys) = unzip $ map fst (Set.toList g)
+    f = (,) <$> minimum <*> maximum
+    (minX, maxX) = f xs
+    (minY, maxY) = f ys
+
+next :: Graph -> Graph
+next = Set.map go
+  where
+    go ((x, y), (vx, vy)) = ((x + vx, y + vy), (vx, vy))
+
+lightParser :: Parser Light
+lightParser = do
+  string "position=<" >> space
+  x <- signedInteger
+  char ',' >> space
+  y <- signedInteger
+  string "> velocity=<" >> space
+  vx <- signedInteger
+  char ',' >> space
+  vy <- signedInteger
+  char '>'
+  return ((x, y), (vx, vy))
 
 day10 :: IO ()
 day10 = do
-  -- input <- readFile "input/input10.txt"
-  return ()
+  input <- Set.fromList . mapMaybe (parseMaybe lightParser) . lines <$> readFile "input/input10.txt"
+  let (n, g) = second head $ calcGraph 0 input
+  putStrLn . unlines . drawGraph (\case Just _ -> '#'; _ -> ' ') . Map.fromSet (const ()) . Set.map fst $ g 
+  print n
--- src/Day11.hs
+++ src/Day11.hs
@@ -1,6 +1,66 @@
 module Day11 where
 
+import Data.Array
+import Data.Bifunctor (Bifunctor (..))
+import Data.Function (on)
+import Data.List (maximumBy)
+import Data.Maybe (fromMaybe)
+import Debug.Trace
+
+input = 9995
+
+type PowerGrid = Array Index Int
+
+type Index = (Int, Int)
+
+initArray :: Int -> PowerGrid
+initArray sn =
+  array
+    b
+    [ ((x, y), p)
+      | (x, y) <- range b,
+        let rackID = x + 10,
+        let p = (((((rackID * y) + sn) * rackID) `div` 100) `mod` 10) - 5
+    ]
+  where
+    b = ((1, 1), (300, 300))
+
+accumGrid :: PowerGrid -> PowerGrid
+accumGrid g = g'
+  where
+    b = bounds g
+    g' =
+      array
+        b
+        [ ((x, y), p x y)
+          | (x, y) <- range b
+        ]
+    p x y = f g' (x - 1, y) + f g' (x, y - 1) + f g (x, y) - f g' (x - 1, y - 1)
+      where
+        f a i = fromMaybe 0 (a !? i)
+
+calcPower :: PowerGrid -> Index -> Index -> Int
+calcPower g i j@(xb, yb) = f j + f i' - f (xa, yb) - f (xb, ya)
+  where
+    i'@(xa, ya) = bimap (subtract 1) (subtract 1) i
+    f i = fromMaybe 0 (g !? i)
+
+a !? i
+  | inRange (bounds a) i = Just (a ! i)
+  | otherwise = Nothing
+
 day11 :: IO ()
 day11 = do
-  -- input <- readFile "input/input11.txt"
-  return ()
+  let g = accumGrid $ initArray input
+  print $ maximumBy (compare `on` snd) $ [((x, y), calcPower g (x, y) (x + 2, y + 2)) | x <- [1 .. 298], y <- [1 .. 298]]
+  print
+    . maximumBy (compare `on` snd)
+    $ [ ((x, y, size), calcPower g (x, y) (x', y'))
+        | x <- [1 .. 300],
+          y <- [1 .. 300],
+          size <- [1 .. 300],
+          let x' = x + size - 1,
+          let y' = y + size - 1,
+          x' <= 300,
+          y' <= 300
+      ]
