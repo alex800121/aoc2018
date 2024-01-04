37d80d11fc3b36eff17adf6ce81e64c94ec164f1
alex800121 <alex800121@hotmail.com>
2024-01-05 03:45:32 +0800

    day9

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,6 +1,21 @@
 module Day5 where
 
+import Data.Char
+
+deletePair :: String -> String
+deletePair = f []
+  where
+    f xs [] = reverse xs
+    f [] (x : xs) = f [x] xs
+    f (x : xs) (y : ys)
+      | isUpper x && toLower x == y = f xs ys
+      | isLower x && toUpper x == y = f xs ys
+      | otherwise = f (y : x : xs) ys
+
+alphaList = [[x, toUpper x] | x <- ['a' .. 'z']]
+
 day5 :: IO ()
 day5 = do
-  -- input <- readFile "input/input5.txt"
-  return ()
+  input <- filter isAlpha <$> readFile "input/input5.txt"
+  print $ length $ deletePair input
+  print $ minimum $ map (\xs -> length $ deletePair $ filter (`notElem` xs) input) alphaList
