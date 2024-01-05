6bb2b622446dd9c4e8a89176e831f7a3f47d2055
alex800121 <alex800121@hotmail.com>
2024-01-05 18:47:26 +0800

    day13

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,6 +1,8 @@
 module Day12 where
 
 import Data.Array
+import Data.Function (on)
+import Data.List (groupBy)
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -37,5 +39,20 @@ day12 = do
   -- g : r : _ <- splitOn "\n\n" <$> readFile "input/test12.txt"
   let initState = map (== '#') $ drop (length "initial state: ") g
       rules = Map.fromList $ map ((\[x, y] -> (x, head y)) . map (map (== '#')) . splitOn " => ") $ lines r
-  putStrLn $ show $ sum $ map (snd . fst) $ filter ((&&) <$> ((== 20) . fst . fst) <*> (id . snd)) $ assocs $ buildGen initState 20 rules
-  -- putStrLn $ show $ sum $ map (snd . fst) $ filter ((&&) <$> ((== 50000000000) . fst . fst) <*> (id . snd)) $ assocs $ buildGen initState 50000000000 rules
+      n200s =
+        map
+          ( \x ->
+              (fst (fst (head x)), sum $ map (snd . fst) $ filter (id . snd) x)
+          )
+          . groupBy ((==) `on` (fst . fst))
+          . assocs
+          $ buildGen initState 200 rules
+      n200a = array (0, 200) n200s
+      n200 = n200a ! 200
+      n199 = n200a ! 199
+      d = n200 - n199
+      n50000000000 = n200 + d * (50000000000 - 200)
+  print $ sum $ map (snd . fst) $ filter ((&&) <$> ((== 20) . fst . fst) <*> snd) $ assocs $ buildGen initState 20 rules
+  print n50000000000
+
+-- putStrLn $ show $ sum $ map (snd . fst) $ filter ((&&) <$> ((== 50000000000) . fst . fst) <*> (id . snd)) $ assocs $ buildGen initState 50000000000 rules
