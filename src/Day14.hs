a1644f83d82f365daf3e5de341c8fbf81d114bed
alex800121 <alex800121@hotmail.com>
2024-01-06 06:13:07 +0800

    day15

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,17 +1,31 @@
+{-# LANGUAGE BangPatterns #-}
 module Day14 where
 
-import Data.Char (digitToInt)
-import qualified Data.Vector.Unboxed as U
-import qualified Data.Vector.Unboxed.Mutable as M
-import Data.Array
+import Data.Char (digitToInt, intToDigit)
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as S
+import Data.List (findIndex, tails, isPrefixOf)
 
-input = 556061
+test = "556061"
+-- test = "59414"
 
-test = 2018
+testInt = read @Int test
 
-buildVector :: Int -> Array Int Int
-buildVector
+testIntList = map digitToInt test
+
+genList :: [Int]
+genList = 3 : 7 : go 0 1 (S.fromList [3, 7])
+  where
+    go !a !b s = next <> go a' b' s'
+      where
+        !iA = s `S.index` a
+        !iB = s `S.index` b
+        !next = map digitToInt (show (iA + iB))
+        s' = s <> S.fromList next
+        a' = (a + iA + 1) `mod` length s'
+        b' = (b + iB + 1) `mod` length s'
 
 day14 :: IO ()
 day14 = do
-  return ()
+  putStrLn $ map intToDigit $ take 10 $ drop testInt genList
+  print $ findIndex (testIntList `isPrefixOf`) $ tails genList
