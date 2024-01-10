5840d973cf1c66ffe1a50fcc2167d2af98ac9a38
alex800121 <alex800121@hotmail.com>
2024-01-11 05:55:49 +0800

    day20

--- src/Day20.hs
+++ src/Day20.hs
@@ -1,6 +1,26 @@
 module Day20 where
 
+import MyLib
+import Text.Megaparsec.Char
+import Text.Megaparsec
+import Data.Char (isAlpha)
+
+type Index = (Int, Int)
+
+unit :: Parser [String]
+unit = do
+  a <- many (divergence <|> direction)
+  return $ foldr (\x acc -> undefined) [] a
+
+direction = many (satisfy isAlpha)
+
+divergence = do
+  char '('
+  a <- unit `sepBy` char '|'
+  char ')'
+  return a
+
 day20 :: IO ()
 day20 = do
-  -- input <- readFile "input/input20.txt"
-  return ()
+  input <- init <$> readFile "input/input20.txt"
+  print input
