dcf3185f2e155280b21f89f20e9692dafc0f4a2f
alex800121 <alex800121@hotmail.com>
2024-01-28 06:53:33 +0900

    done

--- src/Day25.hs
+++ src/Day25.hs
@@ -1,6 +1,23 @@
 module Day25 where
 
+import Data.List.Split (splitOn)
+import Data.List (partition)
+
+manhattan :: [Int] -> [Int] -> Int
+manhattan a = sum . map abs . zipWith subtract a
+
+constellations :: [[Int]] -> [[[Int]]]
+constellations [] = []
+constellations (x : xs) = go [] [] [x] xs
+  where
+    go accAll accCurrent active [] = (active <> accCurrent) : accAll
+    go accAll accCurrent [] (x : xs) = go (accCurrent : accAll) [] [x] xs
+    go accAll accCurrent active xs = go accAll (active <> accCurrent) a b
+      where
+        (a, b) = partition (\x -> any ((<= 3) . manhattan x) active) xs
+
 day25 :: IO ()
 day25 = do
-  -- input <- readFile "input/input25.txt"
-  return ()
+  input <- map (map (read @Int) . splitOn ",") . lines <$> readFile "input/input25.txt"
+  -- input <- map (map (read @Int) . splitOn ",") . lines <$> readFile "input/test25.txt"
+  print $ length $ constellations input
