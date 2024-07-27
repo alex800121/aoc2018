61ad95c4f193e08fb25f1fb271bf8e7580b2ebb1
alex800121 <alex800121@hotmail.com>
2024-07-27 08:05:59 +0800

    day3/5 fix

--- src/Day3.hs
+++ src/Day3.hs
@@ -2,15 +2,15 @@ module Day3 where
 
 import Data.Foldable (Foldable (..), find)
 import Data.Maybe (mapMaybe)
-import Data.MultiSet (MultiSet)
-import qualified Data.MultiSet as MS
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Debug.Trace (traceShow)
 import MyLib (Nat (..), Parser, Vec (..), jointEucVecs, jointEucVecs', overlapEucVec, signedInteger, subtractEucVecs')
 import Paths_AOC2018
 import Text.Megaparsec (parseMaybe)
 import Text.Megaparsec.Char (char, string)
 
-type F = MultiSet (Int, Int)
+type F = Map (Int, Int) Int
 
 data Fabric = Fabric {fid :: Int, fabric :: F}
   deriving (Show, Eq, Ord)
@@ -22,21 +22,21 @@ parseFabric = do
   b <- char ',' *> signedInteger
   c <- string ": " *> signedInteger
   d <- char 'x' *> signedInteger
-  pure $ Fabric i $ MS.fromList [(x, y) | x <- [a .. a + c - 1], y <- [b .. b + d - 1]]
+  pure $ Fabric i $ Map.fromList [((x, y), 1) | x <- [a .. a + c - 1], y <- [b .. b + d - 1]]
 
 day3 :: IO ()
 day3 = do
   input <- mapMaybe (parseMaybe parseFabric) . lines <$> (getDataDir >>= readFile . (++ "/input/input3.txt"))
-  let o = MS.unions $ map fabric input
+  let o = Map.unionsWith (+) $ map fabric input
   putStrLn
     . ("day3a: " ++)
     . show
     . length
     . filter ((> 1) . snd)
-    $ MS.toOccurList o
+    $ Map.toList o
   putStrLn
     . ("day3b: " ++)
     . show
     . fmap fid
-    . find (all ((== 1) . flip MS.occur o) . fabric)
+    . find (all ((== 1) . (o Map.!)) . Map.keysSet . fabric)
     $ input
--- src/Day5.hs
+++ src/Day5.hs
@@ -3,23 +3,23 @@ module Day5 where
 import Data.Char (toUpper)
 import Paths_AOC2018
 
-react :: String -> Int
-react = go 0 ""
+react :: String -> String
+react = go ""
   where
-    go _ [] (y : ys) = go 1 [y] ys
-    go n xs [] = n
-    go n (x : xs) (y : ys) = if toUpper x == toUpper y && x /= y then go (n - 1) xs ys else go (n + 1) (y : x : xs) ys
+    go [] (y : ys) = go [y] ys
+    go xs [] = xs
+    go (x : xs) (y : ys) = if toUpper x == toUpper y && x /= y then go  xs ys else go  (y : x : xs) ys
 
 day5 :: IO ()
 day5 = do
   input <- init <$> (getDataDir >>= readFile . (++ "/input/input5.txt"))
+  let input' = react input
   putStrLn
     . ("day5a: " ++)
-    . show
-    $ react input
+    $ show $ length input'
   putStrLn
     . ("day5b: " ++)
     . show
     . minimum
-    . map (\c -> react (filter (\x -> x /= c && x /= toUpper c) input))
+    . map (\c -> length $ react (filter (\x -> x /= c && x /= toUpper c) input'))
     $ ['a' .. 'z']
