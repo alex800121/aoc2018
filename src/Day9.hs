0c036f57df956126fb1cc263fe1db7f8857d34c6
alex800121 <alex800121@hotmail.com>
2024-08-01 10:58:09 +0800

    day9 1st

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,35 +1,44 @@
 module Day9 where
 
-import Paths_AOC2018
+import Control.Monad.ST (runST)
+import Control.Monad.ST.Strict (ST)
+import Data.CircularList
 import Data.Function (fix)
+import Data.Maybe (fromJust)
+import Data.STRef (modifySTRef', newSTRef, readSTRef, writeSTRef)
+import Data.STRef.Strict (STRef)
+import Data.Sequence
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as M
+import Paths_AOC2018
 
-data CircList a = CircNode
-  { _prev :: CircList a,
-    _node :: a,
-    _next :: CircList a
-  }
+playerN = 452
 
-instance Show a => Show (CircList a) where
-  show (CircNode _ n _) = ".. : " ++ show n ++ " : .."
+lastMarble = 70784
 
-mkCircList :: [a] -> CircList a
-mkCircList [] = error "must have one node"
-mkCircList (x : xs) = last
-  where
-    (last, prev) = go prev x xs last
-    go p x [] r = let l = CircNode p x r in (l, l)
-    go p x (y : ys) r = (l', last)
-      where
-        (r', last) = go l' y  ys r
-        l' = CircNode p x r'
-
-insert :: a -> CircList a -> CircList a
-insert x (CircNode l a r) = fix $ \n -> CircNode (CircNode l a n) x r
--- >>> l = mkCircList [1,2,3,4,5]
--- >>> iterate _prev l !! 99
--- .. : 2 : ..
+playGame :: Int -> Int -> V.Vector Int
+playGame nP nM = runST $ do
+  s <- M.replicate nP (0 :: Int)
+  c <- newSTRef (Data.CircularList.singleton 0)
+  let step i
+        | i `mod` 23 == 0 = do
+            modifySTRef' c (rotNL 7)
+            t <- fromJust . focus <$> readSTRef c
+            M.modify s (+ (i + t)) (i `mod` nP)
+            modifySTRef' c removeR
+        | otherwise = modifySTRef' c (insertL i . rotR)
+  mapM_ step [1 .. nM]
+  V.freeze s
 
 day9 :: IO ()
 day9 = do
-  -- input <- (getDataDir >>= readFile . (++ "/input/input9.txt"))
-  return ()
+  putStrLn
+    . ("day9a: " ++)
+    . show
+    . V.maximum
+    $ playGame playerN lastMarble
+  putStrLn
+    . ("day9a: " ++)
+    . show
+    . V.maximum
+    $ playGame playerN (lastMarble * 100)
